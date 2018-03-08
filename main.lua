6e29ea1afeed6aa73129806a8a6b75134c5e5c20
Desmy MJ <desmy.michaelis-joseph@ocsbstudent.ca>
2018-03-08 11:00:22 -0500

    added code

--- main.lua
+++ main.lua
@@ -3,3 +3,15 @@
 -- Course: ICS2O/3C
 -- This program displays a button that when i click
 -- on it, turns colour and displays text and another
+
+--set background colour
+display.setDefault ("background", 153/255, 204/255, 255/255)
+
+--hide status bar
+display.setStatusBar(display.HiddenStatusBar)
+
+-- create blue button, set it's position and make it visible
+local blueButton = display.newImageRect("Images/Fast Button Inactive@2x.png",198, 96)
+blueButton.x = display.contentWidth/2
+blueButton.y = display.contentHeight/2
+blueButton.isVisible
